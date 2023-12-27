55b90af61ce0cf13af6addb38b2bc987a09feb80
David <dwesley011@gmail.com>
2023-12-27 13:37:19 -0300

    adicionando outras entidades e segunda migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,12 +1,35 @@
-﻿
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Security.Principal;
-
-namespace SalesWebMvc.Models
+﻿namespace SalesWebMvc.Models
 {
-    public class Department // Classe Department | Dados que serão alimentados
+    public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); // Coleção de seller
+
+
+        public Department()
+        {
+            
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void addSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double totalSales(DateTime initial, DateTime final) // calcular o total de vendas do departamento
+        {
+            return Sellers.Sum(x => x.TotalSales(initial, final));
+
+        }
+
+
+
     }
 }
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,60 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime birthDate { get; set; }
+        public double baseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+            
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            this.birthDate = birthDate;
+            this.baseSalary = baseSalary;
+            Department = department;
+        }
+
+
+        public void AddSales(SalesRecord addSales) // Método adicionar vendas
+        {
+            Sales.Add(addSales);
+        }
+
+        public void removeSales(SalesRecord removeSales) // Método remover vendas
+        {
+            Sales.Remove(removeSales);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
