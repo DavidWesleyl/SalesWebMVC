9421d57584d7084f1360d9de721f47bc02da2340
David <dwesley011@gmail.com>
2024-02-21 15:00:09 -0300

    Validando campos

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -6,21 +6,26 @@ namespace SalesWebMvc.Models
     public class Seller // Model Seller | Vendedor
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")] // Vamos colocar a requisição onde os campos forem obrigatórios
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho inválido")]
         public string Name { get; set; }
 
-
+        [Required(ErrorMessage = "{0} Required")]
+        [EmailAddress(ErrorMessage = "Enter a Valid Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
 
-
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime birthDate { get; set; }
 
 
-
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.00, 50000.00, ErrorMessage = "Salary must be min {1} and  max {2}")]
         [Display (Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double baseSalary { get; set; }
